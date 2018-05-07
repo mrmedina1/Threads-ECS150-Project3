e6c708eadd7accf2bed3975c624e086b8a3185d8
smelkris <38511706+smelkris@users.noreply.github.com>
2018-05-06 17:42:44 -0700

    Add files via upload

---
+++ libuthread/sem.c
@@ -0,0 +1,56 @@
+#include <assert.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+#include "queue.h"
+#include "sem.h"
+#include "thread.h"
+
+struct semaphore {
+	/* TODO: Phase 1 */
+  size_t sem_count;
+};
+
+sem_t sem_create(size_t count)
+{
+	/* TODO: Phase 1 */
+  struct semaphore* new_semaphore = malloc(sizeof(struct semaphore));
+  if (new_semaphore == NULL)
+  {
+    return NULL;
+  }
+
+  new_semaphore->sem_count = count;
+  return new_semaphore;
+    
+}
+
+int sem_destroy(sem_t sem)
+{
+	/* TODO: Phase 1 */
+  if (sem == NULL)
+  {
+    return -1;
+  }
+  
+  free(sem);
+  return 0;
+}
+
+int sem_down(sem_t sem)
+{
+	/* TODO: Phase 1 */
+  
+  return 1;
+}
+
+int sem_up(sem_t sem)
+{
+	/* TODO: Phase 1 */
+  return 1;
+}
+
+size_t get_sem_count(sem_t sem)
+{
+  return sem->sem_count;
+}
