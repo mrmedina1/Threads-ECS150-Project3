5296133ba77c1d53b4ec57f053b0c0e40c5f80ee
mrmedina1 <mrmedina@ucdavis.edu>
2018-05-13 17:17:25 -0700

    phase 2.1 works

--- libuthread/tps.c
+++ libuthread/tps.c
@@ -1,173 +1,109 @@
 #include <assert.h>
+#include <limits.h>
 #include <pthread.h>
-#include <signal.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/mman.h>
-#include <unistd.h>
 
-#include "queue.h"
-#include "thread.h"
-#include "tps.h"
+#include <tps.h>
+#include <sem.h>
 
-/* TODO: Phase 2 */
+static char msg1[TPS_SIZE] = "Hello world!\n";
+static char msg2[TPS_SIZE] = "hello world!\n";
 
-struct TPS
+static sem_t sem1, sem2;
+
+void *thread2(void* arg)
 {
-	pthread_t TID;
-	struct memPage *memPage_ptr;
-};
+	char *buffer = malloc(TPS_SIZE);
 
-struct memPage
-{
-	int count;	//Used to keep track of memory pages, needed in write function
-	void* start_ptr;
-};
+	/* Create TPS and initialize with *msg1 */
+	tps_create();
+	tps_write(0, TPS_SIZE, msg1);
 
-struct TPS_STORE
-{
-	queue_t TPS_queue;
-};
+	/* Read from TPS and make sure it contains the message */
+	memset(buffer, 0, TPS_SIZE);
+	tps_read(0, TPS_SIZE, buffer);
+	assert(!memcmp(msg1, buffer, TPS_SIZE));
+	printf("thread2: read OK!\n");
 
-struct TPS_STORE *TPS_store = 0;
+	/* Transfer CPU to thread 1 and get blocked */
+	sem_up(sem1);
+	sem_down(sem2);
 
-//Iterator function to search for TPS
-int tpsSearch(void *data, void *arg)
-{
-	struct TPS *a = (struct TPS*)data;
-	pthread_t match = (pthread_t)arg;
+	/* When we're back, read TPS and make sure it sill contains the original */
+	memset(buffer, 0, TPS_SIZE);
+	tps_read(0, TPS_SIZE, buffer);
+	assert(!memcmp(msg1, buffer, TPS_SIZE));
+	printf("thread2: read OK!\n");
 
-	if (a->TID == match)
-		return 1;
-	return 0;
+	/* Transfer CPU to thread 1 and get blocked */
+	sem_up(sem1);
+	sem_down(sem2);
+
+	/* Destroy TPS and quit */
+	tps_destroy();
+	return NULL;
 }
 
-//Initalizes TPS queue
-int tps_init(int segv)
+void *thread1(void* arg)
 {
-	if (TPS_store != 0) { return -1; }
-	else
-	{
-		TPS_store = malloc(sizeof(struct TPS_STORE));
-		TPS_store -> TPS_queue = queue_create();   
-	}
-	/* TODO: Phase 2 */
-	return 0;
+	pthread_t tid;
+	char *buffer = malloc(TPS_SIZE);
+
+	/* Create thread 2 and get blocked */
+	pthread_create(&tid, NULL, thread2, NULL);
+	sem_down(sem1);
+
+	/* When we're back, clone thread 2's TPS */
+	tps_clone(tid);
+
+	/* Read the TPS and make sure it contains the original */
+	memset(buffer, 0, TPS_SIZE);
+	tps_read(0, TPS_SIZE, buffer);
+	assert(!memcmp(msg1, buffer, TPS_SIZE));
+	printf("thread1: read OK!\n");
+
+	/* Modify TPS to cause a copy on write */
+	buffer[0] = 'h';
+	tps_write(0, 1, buffer);
+
+	/* Transfer CPU to thread 2 and get blocked */
+	sem_up(sem2);
+	sem_down(sem1);
+
+	/* When we're back, make sure our modification is still there */
+	memset(buffer, 0, TPS_SIZE);
+	tps_read(0, TPS_SIZE, buffer);
+	assert(!strcmp(msg2, buffer));
+	printf("thread1: read OK!\n");
+
+	/* Transfer CPU to thread 2 */
+	sem_up(sem2);
+
+	/* Wait for thread2 to die, and quit */
+	pthread_join(tid, NULL);
+	tps_destroy();
+	return NULL;
 }
 
-int tps_create(void)
+int main(int argc, char **argv)
 {
-	/* TODO: Phase 2 */
-	struct TPS *new_tps = malloc(sizeof(struct TPS));
-	if (new_tps == NULL) { return -1; }
-	
-	new_tps -> TID = pthread_self();
-	struct TPS *tps_found = NULL;
-	
-	//Search for TPS using queue_iterate and tpsSearch iterator function
-	queue_iterate(TPS_store -> TPS_queue, tpsSearch, (void*)new_tps -> TID, (void**)&tps_found);
-	if(tps_found != NULL){ return -1; }
-	
-	new_tps -> memPage_ptr = (struct memPage*)malloc(sizeof(struct memPage));
-	if (new_tps -> memPage_ptr == NULL) { return -1; }
-	
-	//PROT_NONE reserves a region of address space for future use
-	//addr ptr NULL, TPS_SIZE = 4096, PRO_NONE, fildescriptor 0, offset 0
-	new_tps -> memPage_ptr -> start_ptr = mmap(NULL, TPS_SIZE, PROT_NONE, MAP_PRIVATE | MAP_ANON, 0, 0);
-	if(new_tps -> memPage_ptr -> start_ptr == MAP_FAILED){ return -1; }
-	
-	new_tps -> memPage_ptr -> count = 1;
+	pthread_t tid;
 
-	queue_enqueue(TPS_store -> TPS_queue, new_tps);
-  
-	return 0;
-}
+	/* Create two semaphores for thread synchro */
+	sem1 = sem_create(0);
+	sem2 = sem_create(0);
 
-int tps_destroy(void)
-{
-	/* TODO: Phase 2 */
-	struct TPS *tps_found = NULL;
-	pthread_t tid = (pthread_t)pthread_self();
-	queue_iterate(TPS_store -> TPS_queue, tpsSearch, (void*)tid, (void**)&tps_found);
-	if(tps_found == NULL){ return -1; }
-	
-	assert(queue_delete(TPS_store -> TPS_queue, tps_found) != -1);
-	
-	return 0;
-}
+	/* Init TPS API */
+	tps_init(1);
 
-int tps_read(size_t offset, size_t length, char *buffer)
-{
-	/* TODO: Phase 2 */
-	int outOfBounds = (length + offset);
-	
-	struct TPS *tps_found = NULL;
-	pthread_t tid = (pthread_t)pthread_self();
-	queue_iterate(TPS_store -> TPS_queue, tpsSearch, (void*)tid, (void**)&tps_found);
-	if(tps_found == NULL || outOfBounds > TPS_SIZE || buffer == NULL){ return -1; }
-	
-	//Copy data
-	memcpy(buffer, tps_found->memPage_ptr->start_ptr + offset, length);
-	
-	//Assert data copy was successful
-	assert(strcmp(tps_found->memPage_ptr->start_ptr + offset, buffer) == 0);
-	
-	return 0;
-}
+	/* Create thread 1 and wait */
+	pthread_create(&tid, NULL, thread1, NULL);
+	pthread_join(tid, NULL);
 
-int tps_write(size_t offset, size_t length, char *buffer)
-{
-	/* TODO: Phase 2 */
-	int outOfBounds = (length + offset);
-	
-	struct TPS *tps_found = NULL;
-	pthread_t tid = (pthread_t)pthread_self();
-	queue_iterate(TPS_store -> TPS_queue, tpsSearch, (void*)tid, (void**)&tps_found);
-	if(tps_found == NULL || outOfBounds > TPS_SIZE || buffer == NULL){ return -1; }
-
-	if(tps_found -> memPage_ptr -> count > 1)
-	{
-		struct memPage *new_memPage = (struct memPage*)malloc(sizeof(struct memPage));
-		tps_found -> memPage_ptr -> count -= 1;
-		new_memPage -> start_ptr =  mmap(NULL, TPS_SIZE, PROT_NONE, MAP_PRIVATE | MAP_ANON, 0, 0);
-		
-		memcpy(new_memPage -> start_ptr, tps_found -> memPage_ptr -> start_ptr, TPS_SIZE);
-		
-		tps_found -> memPage_ptr = new_memPage;
-		tps_found -> memPage_ptr -> count = 1;
-	}
-
-	return 0;
-}
-
-//Handles ptr redirection
-int tps_clone(pthread_t tid)
-{
-	/* TODO: Phase 2 */
-	struct TPS *tps_found = NULL;
-	queue_iterate(TPS_store -> TPS_queue, tpsSearch, (void*)tid, (void**)&tps_found);
-	
-	struct TPS *tps_current_found = NULL;
-	pthread_t tid_current = (pthread_t)pthread_self();
-	queue_iterate(TPS_store -> TPS_queue, tpsSearch, (void*)tid_current, (void**)&tps_current_found);
-	
-	if(tps_found == NULL || tps_current_found != NULL){ return -1; }
-	
-	struct TPS *cloned_tps = (struct TPS*)malloc(sizeof(struct TPS));
-	if(cloned_tps == NULL){ return -1; }
-	
-	//Clone the TPS.  memPage pointer is reassigned
-	cloned_tps -> memPage_ptr = (struct memPage*)malloc(sizeof(struct memPage));
-	cloned_tps -> TID = tid_current;
-	cloned_tps -> memPage_ptr = tps_found -> memPage_ptr;
-	
-	//Increment memory page counter
-	tps_found -> memPage_ptr -> count += 1;
-	
-	//Enqueue cloned TPS
-	queue_enqueue(TPS_store -> TPS_queue, cloned_tps);
-
+	/* Destroy resources and quit */
+	sem_destroy(sem1);
+	sem_destroy(sem2);
 	return 0;
 }
