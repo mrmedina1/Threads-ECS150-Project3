d5553f3085eb46366f49c889a2d92a25d69ffc06
mrmedina1 <mrmedina@ucdavis.edu>
2018-05-15 20:18:36 -0700

    Commentation good

--- libuthread/tps.c
+++ libuthread/tps.c
@@ -12,20 +12,21 @@
 #include "thread.h"
 #include "tps.h"
 
-/* TODO: Phase 2 */
-
+//Struct to hold TPS information
 struct TPS
 {
 	pthread_t TID;
 	struct memPage *memPage_ptr;
 };
 
+//Struct for storing memory pages
 struct memPage
 {
 	int count;	//Used to keep track of memory pages, needed in write function
 	void* start_ptr;
 };
 
+//TPS queue struct
 struct TPS_STORE
 {
 	queue_t TPS_queue;
@@ -44,32 +45,33 @@ int tpsSearch(void *data, void *arg)
 	return 0;
 }
 
+//Iterator function to search for memory page address
 int pageAddressSearch(void *data, void *arg)
 {
-  struct TPS *a = (struct TPS*)data;
-  void* match = (void*) arg;
-  if (a->memPage_ptr->start_ptr == match)
-    return 1;
-  return 0;
+	struct TPS *a = (struct TPS*)data;
+	void* match = (void*) arg;
+	if (a->memPage_ptr->start_ptr == match)
+		return 1;
+	return 0;
 }
 
+//Signal handler function to distinguish between actual segmentation faults and TPS protection faults
 static void segv_handler(int sig, siginfo_t *si, void *context)
 {
-  void *p_fault = (void*)((uintptr_t)si->si_addr & ~(TPS_SIZE - 1));
+	void *p_fault = (void*)((uintptr_t)si->si_addr & ~(TPS_SIZE - 1));
 	
 	void *found = NULL;
-  queue_iterate(TPS_store->TPS_queue, pageAddressSearch, p_fault, (void**)&found);
+	queue_iterate(TPS_store->TPS_queue, pageAddressSearch, p_fault, (void**)&found);
     
-  if (found != NULL)
-  {
-    fprintf(stderr, "TPS protection error! \n");
-  }
+	if (found != NULL)
+	{
+		fprintf(stderr, "TPS protection error! \n");
+	}
   
-  signal(SIGSEGV, SIG_DFL);
-  signal(SIGBUS, SIG_DFL);
+	signal(SIGSEGV, SIG_DFL);
+	signal(SIGBUS, SIG_DFL);
 
-  raise(sig);
-
+	raise(sig);
 }
 
 //Initalizes TPS queue
@@ -81,31 +83,33 @@ int tps_init(int segv)
 		TPS_store = malloc(sizeof(struct TPS_STORE));
 		TPS_store -> TPS_queue = queue_create();   
 	}
-	/* TODO: Phase 2 */
-  if (segv) 
-  {
-    struct sigaction sa;
+	
+	//Checks for segfault
+	if (segv) 
+	{
+		struct sigaction sa;
   
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_SIGINFO;
-    sa.sa_sigaction = segv_handler;
-    sigaction(SIGBUS, &sa, NULL);
-    sigaction(SIGSEGV, &sa, NULL);
-  }
+		sigemptyset(&sa.sa_mask);
+		sa.sa_flags = SA_SIGINFO;
+		sa.sa_sigaction = segv_handler;
+		sigaction(SIGBUS, &sa, NULL);
+		sigaction(SIGSEGV, &sa, NULL);
+	}
 	return 0;
 }
 
-
+//Creates a new TPS
 int tps_create(void)
 {
-	/* TODO: Phase 2 */
+	
 	struct TPS *new_tps = malloc(sizeof(struct TPS));
 	if (new_tps == NULL) { return -1; }
 	
+	//Get TID.
+	//Search for TPS using queue_iterate and tpsSearch iterator function
+	//Return error if TPS not found
 	new_tps -> TID = pthread_self();
 	struct TPS *tps_found = NULL;
-	
-	//Search for TPS using queue_iterate and tpsSearch iterator function
 	queue_iterate(TPS_store -> TPS_queue, tpsSearch, (void*)new_tps -> TID, (void**)&tps_found);
 	if(tps_found != NULL){ return -1; }
 	
@@ -117,16 +121,17 @@ int tps_create(void)
 	new_tps -> memPage_ptr -> start_ptr = mmap(NULL, TPS_SIZE, PROT_NONE, MAP_PRIVATE | MAP_ANON, 0, 0);
 	if(new_tps -> memPage_ptr -> start_ptr == MAP_FAILED){ return -1; }
 	
+	//Increment memory page count and enqueue the new TPS
 	new_tps -> memPage_ptr -> count = 1;
-
 	queue_enqueue(TPS_store -> TPS_queue, new_tps);
   
 	return 0;
 }
 
+//Destroys a TPS
 int tps_destroy(void)
 {
-	/* TODO: Phase 2 */
+	//Get TID and search the TPS queue for a TID match.  Return error if TPS not found
 	struct TPS *tps_found = NULL;
 	pthread_t tid = (pthread_t)pthread_self();
 	queue_iterate(TPS_store -> TPS_queue, tpsSearch, (void*)tid, (void**)&tps_found);
@@ -134,20 +139,28 @@ int tps_destroy(void)
 	
 	//assert(queue_delete(TPS_store -> TPS_queue, tps_found) != -1);
 	
+	//Delete TPS from queue
+	queue_delete(TPS_store -> TPS_queue, tps_found);
+	
 	return 0;
 }
 
+//Read a certain amount of bytes at offset of current threads TPS
 int tps_read(size_t offset, size_t length, char *buffer)
 {
-	/* TODO: Phase 2 */
 	int outOfBounds = (length + offset);
 	
+	//Get TID and search the TPS queue for a TID match
 	struct TPS *tps_found = NULL;
 	pthread_t tid = (pthread_t)pthread_self();
 	queue_iterate(TPS_store -> TPS_queue, tpsSearch, (void*)tid, (void**)&tps_found);
+	
+	//Error checks for TID not found in queue, outOfBounds index on TPS size, 
+	//or an empty buffer.
 	if(tps_found == NULL || outOfBounds > TPS_SIZE || buffer == NULL){ return -1; }
 	
-	//Copy data
+	//Set the found TPS protections to read, copy the data, then reset
+	//protections so that the memory cannot be accessed
 	mprotect(tps_found -> memPage_ptr -> start_ptr, TPS_SIZE, PROT_READ);
 	memcpy(buffer, tps_found -> memPage_ptr -> start_ptr + offset, length);
 	mprotect(tps_found -> memPage_ptr -> start_ptr, TPS_SIZE, PROT_NONE);
@@ -158,32 +171,46 @@ int tps_read(size_t offset, size_t length, char *buffer)
 	return 0;
 }
 
+//Write a certain amount of bytes at offset of current threads TPS
 int tps_write(size_t offset, size_t length, char *buffer)
 {
-	/* TODO: Phase 2 */
 	int outOfBounds = (length + offset);
 	
+	//Get TID and search the TPS queue for a TID match
 	struct TPS *tps_found = NULL;
 	pthread_t tid = (pthread_t)pthread_self();
 	queue_iterate(TPS_store -> TPS_queue, tpsSearch, (void*)tid, (void**)&tps_found);
+	
+	//Error checks for TID not found in queue, outOfBounds index on TPS size, 
+	//or an empty buffer.
 	if(tps_found == NULL || outOfBounds > TPS_SIZE || buffer == NULL){ return -1; }
 
+	//Check if the found TPS has been cloned
 	if(tps_found -> memPage_ptr -> count > 1)
 	{
 		struct memPage *new_memPage = (struct memPage*)malloc(sizeof(struct memPage));
 		tps_found -> memPage_ptr -> count -= 1;
 		new_memPage -> start_ptr =  mmap(NULL, TPS_SIZE, PROT_NONE, MAP_PRIVATE | MAP_ANON, 0, 0);
 		
+		//Set read protection for the found TPS and write protections for the
+		//new memory page
 		mprotect(tps_found -> memPage_ptr -> start_ptr, TPS_SIZE, PROT_READ);
 		mprotect(new_memPage -> start_ptr, TPS_SIZE, PROT_WRITE);
+		
+		//Copy the found TPS memory page to the new memory page
 		memcpy(new_memPage -> start_ptr, tps_found -> memPage_ptr -> start_ptr, TPS_SIZE);
+		
+		//Set protections back so that the memory cannot be accessed
 		mprotect(tps_found -> memPage_ptr -> start_ptr, TPS_SIZE, PROT_NONE);
 		mprotect(new_memPage -> start_ptr, TPS_SIZE, PROT_NONE);
 		
+		//Readjust the found TPS pointer and increment the memory page count
 		tps_found -> memPage_ptr = new_memPage;
 		tps_found -> memPage_ptr -> count = 1;
 	}
 
+	//Set the found TPS protections to write, copy the data, then reset
+	//protections so that the memory cannot be accessed
 	mprotect(tps_found -> memPage_ptr -> start_ptr, TPS_SIZE, PROT_WRITE);
 	memcpy(tps_found -> memPage_ptr -> start_ptr + offset, buffer, length);
 	mprotect(tps_found -> memPage_ptr -> start_ptr, TPS_SIZE, PROT_NONE);
@@ -191,17 +218,20 @@ int tps_write(size_t offset, size_t length, char *buffer)
 	return 0;
 }
 
-//Handles ptr redirection
+//Clone 'tid' parameter's TPS
+//Handles ptr redirection rather than using memcpy
 int tps_clone(pthread_t tid)
 {
-	/* TODO: Phase 2 */
+	//Search for tps of 'tid' parameter
 	struct TPS *tps_found = NULL;
 	queue_iterate(TPS_store -> TPS_queue, tpsSearch, (void*)tid, (void**)&tps_found);
 	
+	//Search for the current TID
 	struct TPS *tps_current_found = NULL;
 	pthread_t tid_current = (pthread_t)pthread_self();
 	queue_iterate(TPS_store -> TPS_queue, tpsSearch, (void*)tid_current, (void**)&tps_current_found);
 	
+	//Return error if 'tid' parameter wasn't found, or if current TID was found
 	if(tps_found == NULL || tps_current_found != NULL){ return -1; }
 	
 	struct TPS *cloned_tps = (struct TPS*)malloc(sizeof(struct TPS));
@@ -212,10 +242,8 @@ int tps_clone(pthread_t tid)
 	cloned_tps -> TID = tid_current;
 	cloned_tps -> memPage_ptr = tps_found -> memPage_ptr;
 	
-	//Increment memory page counter
+	//Increment memory page counter and enqueue the cloned TPS
 	tps_found -> memPage_ptr -> count += 1;
-	
-	//Enqueue cloned TPS
 	queue_enqueue(TPS_store -> TPS_queue, cloned_tps);
 
 	return 0;
